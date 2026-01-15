941e70cfdedeeab75532c295840c2e019c93f0dc
Tapas <borj18237@gmail.com>
2026-01-15 07:55:37 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -33,7 +33,7 @@ export default function RoomDetailPage({ params }: { params: { id: string } }) {
     if (params.id) {
       fetchRoom(params.id);
     }
-  }, [params]);
+  }, [params.id]);
 
   if (isLoading) {
     return (
